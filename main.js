0e83bdc190011ebebc2695471ff21312361cd2d6
Modepesawat19 <sofyanhariri19@gmail.com>
2022-08-08 04:43:36 +0700

    kommit

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
-let cookieModal = document.querySelector(".sliding-panel")
-let closeCookieBtn = document.querySelector(".close.btn")
+
+let cookieModal = document.querySelector(".popup")
+let closeCookieBtn = document.querySelector(".close-btn")
 let countCookiebtn = document.querySelector(".btn-count")
-
-
 
 countCookiebtn.addEventListener("click", function (){
     cookieModal.classList.remove("active")
     localStorage.setItem("cookieAccept", "yes")
 })
 
+closeCookieBtn.addEventListener("click", function (){
+    cookieModal.classList.remove("active")
+    localStorage.setItem("cookieAccept", "tooglePopup")
+})
+
 
 setTimeout(function (){
     cookieModal.classList.add("active")
-},2000)
+},6000)
