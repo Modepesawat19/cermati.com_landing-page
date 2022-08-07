7ab4aae7928cafbc0e1c130d74a8bae12c3f141b
Modepesawat19 <sofyanhariri19@gmail.com>
2022-08-07 12:59:38 +0700

    komit

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-// console.log('tai')
+let cookieModal = document.querySelector(".sliding-panel")
+
+let countCookiebtn = document.querySelector(".btn-count")
+
+
+
+countCookiebtn.addEventListener("click", function (){
+    cookieModal.classList.remove("active")
+    localStorage.setItem("cookieAccept", "yes")
+})
+
+
+setTimeout(function (){
+    cookieModal.classList.add("active")
+},2000)
